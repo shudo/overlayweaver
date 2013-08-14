33dcfc74a53a2542544a4ab973b426ade9c25e27
Kazuyuki Shudo <shudo@computer.org>
2013-08-14 23:03:47 +0900

    Jetty was updated from 9.0.3 to 9.0.4. Berkeley DB Java Edition was updated from 5.0.73 to 5.0.84.

--- bin/owdhtshell.bat
+++ bin/owdhtshell.bat
@@ -6,7 +6,7 @@ set LIB_DIR=%OW_HOME%\lib
 set TARGET_DIR=%OW_HOME%\target
 set BUILD_DIR=%OW_HOME%\build
 
-set CLASSPATH=%BUILD_DIR%;%TARGET_DIR%\overlayweaver.jar;%LIB_DIR%\je-5.0.73.jar;%LIB_DIR%\xmlrpc-common-3.1.3.jar;%LIB_DIR%\xmlrpc-server-3.1.3.jar;%LIB_DIR%\ws-commons-util-1.0.2.jar;%LIB_DIR%\commons-cli-1.2.jar;%LIB_DIR%\servlet-api-3.0.jar;%LIB_DIR%\jetty-server-9.0.3.v20130506.jar;%LIB_DIR%\jetty-servlet-9.0.3.v20130506.jar;%LIB_DIR%\jetty-util-9.0.3.v20130506.jar;%LIB_DIR%\jetty-http-9.0.3.v20130506.jar;%LIB_DIR%\jetty-security-9.0.3.v20130506.jar;%LIB_DIR%\jetty-io-9.0.3.v20130506.jar;%LIB_DIR%\jetty-continuation-9.0.3.v20130506.jar;%LIB_DIR%\clink200.jar
+set CLASSPATH=%BUILD_DIR%;%TARGET_DIR%\overlayweaver.jar;%LIB_DIR%\je-5.0.84.jar;%LIB_DIR%\xmlrpc-common-3.1.3.jar;%LIB_DIR%\xmlrpc-server-3.1.3.jar;%LIB_DIR%\ws-commons-util-1.0.2.jar;%LIB_DIR%\commons-cli-1.2.jar;%LIB_DIR%\servlet-api-3.0.jar;%LIB_DIR%\jetty-server-9.0.4.v20130625.jar;%LIB_DIR%\jetty-servlet-9.0.4.v20130625.jar;%LIB_DIR%\jetty-util-9.0.4.v20130625.jar;%LIB_DIR%\jetty-http-9.0.4.v20130625.jar;%LIB_DIR%\jetty-security-9.0.4.v20130625.jar;%LIB_DIR%\jetty-io-9.0.4.v20130625.jar;%LIB_DIR%\jetty-continuation-9.0.4.v20130625.jar;%LIB_DIR%\clink200.jar
 set LOGGING_CONFIG="%BIN_DIR%\logging.properties"
 
 set JVM_OPTION=-Xss80k
