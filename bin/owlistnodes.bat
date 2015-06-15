5eca067b52ea2aca12708b6e46ec96de50075496
Kazuyuki Shudo <shudo@computer.org>
2015-06-16 03:08:21 +0900

    Apache Commons CLI was updated from 1.2 to 1.3.

--- bin/owlistnodes.bat
+++ bin/owlistnodes.bat
@@ -6,7 +6,7 @@ set LIB_DIR=%OW_HOME%\lib
 set TARGET_DIR=%OW_HOME%\target
 set BUILD_DIR=%OW_HOME%\build
 
-set CLASSPATH=%BUILD_DIR%;%TARGET_DIR%\overlayweaver.jar;%LIB_DIR%\commons-cli-1.2.jar
+set CLASSPATH=%BUILD_DIR%;%TARGET_DIR%\overlayweaver.jar;%LIB_DIR%\commons-cli-1.3.jar
 set LOGGING_CONFIG="%BIN_DIR%\logging.properties"
 
 java -Djava.util.logging.config.file=%LOGGING_CONFIG% ow.tool.listnodes.Main %*
