d4ab1a665459eb1bd77a6777410686a88e66ca15
Kazuyuki Shudo <shudo@computer.org>
2015-11-25 22:37:21 +0900

    Berkeley DB Java Edition was updated from 6.3.8 to 6.4.9.

--- bin/owemu.bat
+++ bin/owemu.bat
@@ -6,7 +6,7 @@ set LIB_DIR=%OW_HOME%\lib
 set TARGET_DIR=%OW_HOME%\target
 set BUILD_DIR=%OW_HOME%\build
 
-set CLASSPATH=%BUILD_DIR%;%BUILD_DIR%\main;%BUILD_DIR%\test;%TARGET_DIR%\overlayweaver.jar;%LIB_DIR%\je-6.3.8.jar;%LIB_DIR%\xmlrpc-common-3.1.3.jar;%LIB_DIR%\xmlrpc-server-3.1.3.jar;%LIB_DIR%\ws-commons-util-1.0.2.jar;%LIB_DIR%\commons-cli-1.3.1.jar
+set CLASSPATH=%BUILD_DIR%;%BUILD_DIR%\main;%BUILD_DIR%\test;%TARGET_DIR%\overlayweaver.jar;%LIB_DIR%\je-6.4.9.jar;%LIB_DIR%\xmlrpc-common-3.1.3.jar;%LIB_DIR%\xmlrpc-server-3.1.3.jar;%LIB_DIR%\ws-commons-util-1.0.2.jar;%LIB_DIR%\commons-cli-1.3.1.jar
 set LOGGING_CONFIG="%BIN_DIR%\logging.properties"
 
 set JVM_OPTION=-Xss100k
